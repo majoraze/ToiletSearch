7aea5093718b55d62dbfeb58af20421b05931f4a
Pedro Rodrigues <majoraze@gmail.com>
2015-02-01 17:40:45 -0200

    alteracoes simples na forma de exibicao

--- toilet-search/app/controllers/bathrooms_controller.rb
+++ toilet-search/app/controllers/bathrooms_controller.rb
@@ -18,23 +18,36 @@ class BathroomsController < ApplicationController
   end
 
   def new
-
+    #creates the structure to the toilet and the chance to the user who created to make a review
+    @bathroom = Bathroom.new
+    @bathroom.feature = Feature.new
+    @bathroom.reviews = [Review.new] #como é has_many e só quero cadastrar 1, tenho que iniciar na array
   end
 
   def edit
-
+    #cannot edit
   end
 
   def create
+    @bathroom = Bathroom.new(bathroom_params)
+    @bathroom.save
 
+    redirect_to root_path, {notice: "Criado!"}
+    #render text: params[:bathroom].inspect
   end
 
   def update
-
+    #cannot update
   end
 
   def destroy
+    #cannot destroy
+  end
 
+  private
+  def bathroom_params
+    #depois separar as permissões para impedir injection, aqui to forçando a permissão de tudo
+    params.require(:bathroom).permit!
   end
 
 end
--- toilet-search/app/models/bathroom.rb
+++ toilet-search/app/models/bathroom.rb
@@ -3,4 +3,6 @@ class Bathroom < ActiveRecord::Base
   has_many :reviews, dependent: :destroy
   has_one :feature, dependent: :destroy
   belongs_to :category
+  accepts_nested_attributes_for :reviews
+  accepts_nested_attributes_for :feature
 end
